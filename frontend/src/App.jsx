123b0082fc5f3e3fbe01f348c908ecf835814b05
alandiegobiz <adiegobiz@gmail.com>
2021-01-12 14:33:29 -0300

    Desenvolvimento da pagina Home com textos e imagens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+
 import Home from './pages/Home'
+
+import './assets/styles/global.css';
 
 const App = () => {
     return (
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 
+import './style.css';
+
+import logo_sinalcode from '../../assets/images/Logos/logo_sinalcode.png'
+
 const Home = () => {
     return (
-        <div>
-            <h1>Sinal Code</h1>
+        
+        <div className="section-1">
+            <div className="container">
+                <header className="header">
+                    <img src={logo_sinalcode} alt="Sinal Code - Logo" />
+                    <nav>
+                        <button>QUEM SOMOS</button>
+                        <button>CURSOS</button>
+                        <button>FALE CONOSCO</button>
+                        <button>FAZER LOGIN</button>
+                    </nav>
+                </header>
+                <main>
+                    <section>
+                        <h1>Curso de programação para surdos</h1>
+                        <p>
+                            A sinal Code desenvolveu uma plataforma digital de cursos na área de <br />
+                            programação elaborados para atender exclusivamente a comunidade surda.
+                        </p>
+                        <p>
+                            Todas as aulas são adaptadas com a língua de sinais (libras) e legenda, <br />
+                            para um melhor desenvolvimento no ensino-aprendizagem.<br />
+                        </p>
+                        <p>
+                            O curso ofertado é 100% gratuito. Contamos com o patrocínio de empresas <br />
+                            parceiras nesta empreitada de cunho social.<br />
+                        </p>
+                    </section>
+                    <section className="estudar">
+                        <h1>Por que estudar programação?</h1>
+                    </section>
+                </main>
+            </div>
         </div>
+        
     )
 }
 
